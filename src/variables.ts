bdeda9ab7b9f5d01095f0e34f30f8dc09d36aa07
Julian Waller <git@julusian.co.uk>
2022-01-04 18:11:18 +0000

    chore: fix type errors

--- src/variables.ts
+++ src/variables.ts
@@ -6,8 +6,8 @@ import { DeviceConfig } from './config'
 export function updateVariables(instance: InstanceSkel<DeviceConfig>, state: HassEntities): void {
 	const variables: { [variableId: string]: string | undefined } = {}
 	for (const entity of Object.values(state)) {
-		variables[`entity.${entity.entity_id}.value`] = entity.state;
-		variables[`entity.${entity.entity_id}`] = entity.attributes.friendly_name ?? entity.entity_id;
+		variables[`entity.${entity.entity_id}.value`] = entity.state
+		variables[`entity.${entity.entity_id}`] = entity.attributes.friendly_name ?? entity.entity_id
 	}
 
 	instance.setVariables(variables)
@@ -18,13 +18,13 @@ export function InitVariables(instance: InstanceSkel<DeviceConfig>, state: HassE
 
 	for (const entity of Object.values(state)) {
 		variables.push({
-			label: `Entity: ${(_a = entity.attributes.friendly_name) !== null && _a !== void 0 ? _a : entity.entity_id}`,
+			label: `Entity Value: ${entity.attributes.friendly_name ?? entity.entity_id}`,
 			name: `entity.${entity.entity_id}.value`,
-		});
+		})
 		variables.push({
-			label: `Entity: ${entity.attributes.friendly_name ?? entity.entity_id}`,
+			label: `Entity Name: ${entity.attributes.friendly_name ?? entity.entity_id}`,
 			name: `entity.${entity.entity_id}`,
-		});
+		})
 	}
 	instance.setVariableDefinitions(variables)
 }
