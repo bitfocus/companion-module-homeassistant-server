5754c2575847c4f996d7fd7f6d79c2283bd66a36
Julian Waller <git@julusian.co.uk>
2022-01-04 18:40:04 +0000

    feat: add set light brightness

--- src/actions.ts
+++ src/actions.ts
@@ -1,13 +1,14 @@
 import { Connection, HassEntities } from 'home-assistant-js-websocket'
 import { CompanionActionEvent, CompanionActions, CompanionAction } from '../../../instance_skel_types'
-import { EntityPicker, OnOffTogglePicker, PctText } from './choices'
+import { EntityPicker, OnOffTogglePicker } from './choices'
 import { OnOffToggle } from './util'
 
 export enum ActionId {
 	SetSwitch = 'set_switch',
 	SetInputBoolean = 'set_input_boolean',
 	SetLightOn = 'set_light_on',
-	AdjLightPct = 'adj_light_pct',
+	SetLightPercent = 'set_light_pct',
+	AdjLightPercent = 'adj_light_pct',
 	ExecuteScript = 'execute_script',
 	PressButton = 'press_button',
 }
@@ -22,44 +23,33 @@ export function GetActionsList(
 
 		const entity = state[String(opt.entity_id)]
 		if (entity) {
-			let newState: string
+			let service: string
 			switch (opt.state as OnOffToggle) {
 				case OnOffToggle.Off:
-					newState = 'turn_off'
+					service = 'turn_off'
 					break
 				case OnOffToggle.Toggle:
-					newState = entity.state === 'off' ? 'turn_on' : 'turn_off'
+					service = 'toggle'
 					break
 				default:
-					newState = 'turn_on'
+					service = 'turn_on'
 					break
 			}
 
 			client?.sendMessage({
 				type: 'call_service',
 				domain: 'homeassistant',
-				service: newState,
+				service: service,
 				service_data: {
 					entity_id: [opt.entity_id],
 				},
 			})
 		}
 	}
-	const entityAdj = (opt: CompanionActionEvent['options']): void => {
-		const { client } = getProps()
-
-		client?.sendMessage({
-			type: 'call_service',
-			domain: 'homeassistant',
-			service: 'turn_on',
-			service_data: {
-				entity_id: opt.entity_id,
-				brightness_step_pct: Number(opt.pct),
-			},
-		})
-	}
 
 	const { state: initialState } = getProps()
+	const pickerLights = EntityPicker(initialState, 'light')
+
 	const actions: { [id in ActionId]: CompanionActionWithCallback | undefined } = {
 		[ActionId.SetSwitch]: {
 			label: 'Set switch state',
@@ -73,13 +63,65 @@ export function GetActionsList(
 		},
 		[ActionId.SetLightOn]: {
 			label: 'Set light on/off state',
-			options: [EntityPicker(initialState, 'light'), OnOffTogglePicker()],
+			options: [pickerLights, OnOffTogglePicker()],
 			callback: (evt): void => entityOnOff(evt.options),
 		},
-		[ActionId.AdjLightPct]: {
+		[ActionId.SetLightPercent]: {
+			label: 'Set light brightness (percentage)',
+			options: [
+				pickerLights,
+				{
+					type: 'number',
+					label: 'Adjustment',
+					id: 'pct',
+					default: 50,
+					min: 0,
+					max: 100,
+					step: 1,
+					range: true,
+				},
+			],
+			callback: (evt): void => {
+				const { client } = getProps()
+
+				client?.sendMessage({
+					type: 'call_service',
+					domain: 'light',
+					service: 'turn_on',
+					service_data: {
+						entity_id: evt.options.entity_id,
+						brightness_pct: Number(evt.options.pct),
+					},
+				})
+			},
+		},
+		[ActionId.AdjLightPercent]: {
 			label: 'Adjust light brightness (percentage)',
-			options: [EntityPicker(initialState, 'light'), PctText()],
-			callback: (evt): void => entityAdj(evt.options),
+			options: [
+				pickerLights,
+				{
+					type: 'number',
+					label: 'Adjustment',
+					id: 'pct',
+					default: 1,
+					min: -100,
+					max: 100,
+					step: 1,
+				},
+			],
+			callback: (evt): void => {
+				const { client } = getProps()
+
+				client?.sendMessage({
+					type: 'call_service',
+					domain: 'light',
+					service: 'turn_on',
+					service_data: {
+						entity_id: evt.options.entity_id,
+						brightness_step_pct: Number(evt.options.pct),
+					},
+				})
+			},
 		},
 		[ActionId.ExecuteScript]: {
 			label: 'Execute script',
--- src/variables.ts
+++ src/variables.ts
@@ -1,21 +1,23 @@
 import { HassEntities } from 'home-assistant-js-websocket'
 import InstanceSkel = require('../../../instance_skel')
 import { CompanionVariable } from '../../../instance_skel_types'
+import { LIGHT_MAX_BRIGHTNESS } from './choices'
 import { DeviceConfig } from './config'
 
 export function updateVariables(instance: InstanceSkel<DeviceConfig>, state: HassEntities): void {
-	const variables: { [variableId: string]: string | undefined } = {}
+	const variables: { [variableId: string]: string | number | undefined } = {}
 	for (const entity of Object.values(state)) {
 		variables[`entity.${entity.entity_id}.value`] = entity.state
 		variables[`entity.${entity.entity_id}`] = entity.attributes.friendly_name ?? entity.entity_id
-		if (entity.entity_id.indexOf('light.') != -1) {
-			variables[`entity.${entity.entity_id}.brightness`] = String(
-				Math.round((100 * (entity.attributes.brightness ?? 0)) / 256)
+
+		if (entity.entity_id.startsWith('light.')) {
+			variables[`entity.${entity.entity_id}.brightness`] = Math.round(
+				(100 * (entity.attributes.brightness ?? 0)) / LIGHT_MAX_BRIGHTNESS
 			)
 		}
 	}
 
-	instance.setVariables(variables)
+	instance.setVariables(variables as any) // TODO - remove this cast
 }
 
 export function InitVariables(instance: InstanceSkel<DeviceConfig>, state: HassEntities): void {
@@ -30,9 +32,10 @@ export function InitVariables(instance: InstanceSkel<DeviceConfig>, state: HassE
 			label: `Entity Name: ${entity.attributes.friendly_name ?? entity.entity_id}`,
 			name: `entity.${entity.entity_id}`,
 		})
-		if (entity.entity_id.indexOf('light.') != -1) {
+
+		if (entity.entity_id.startsWith('light.')) {
 			variables.push({
-				label: `${entity.attributes.friendly_name ?? entity.entity_id} brightness`,
+				label: `Light Brightness: ${entity.attributes.friendly_name ?? entity.entity_id}`,
 				name: `entity.${entity.entity_id}.brightness`,
 			})
 		}
